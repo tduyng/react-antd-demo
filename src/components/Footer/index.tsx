f34cd87a7859645b1d5e04dbcae142f67f688359
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-21 18:23:41 +0100

    Update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import { BackTop } from 'antd';
 
 export const AppFooter = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="container-fluid">
+      <div className="footer">
+        <div className="logo">
+          <i className="fas fa-bolt"></i>
+          <a href="/">Tech</a>
+        </div>
+        <ul className="socials">
+          <li>
+            <a href="https://facebook.com">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.pinterest.com/">
+              <i className="fab fa-pinterest-p"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+        <div className="copyright">Copyright &copy; 2020 Tech</div>
+        <BackTop>
+          <div className="go-top">
+            <i className="fas fa-arrow-circle-up"></i>
+          </div>
+        </BackTop>
+      </div>
+    </div>
+  );
 };
