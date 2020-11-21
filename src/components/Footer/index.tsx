3c0294d717241a3440857295513c9d65c24114c7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-21 11:25:58 +0100

    Update scss for body Home page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export const Footer = () => {
+export const AppFooter = () => {
   return <div>Footer</div>;
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export const Header = () => {
+export const AppHeader = () => {
   return <div>Header</div>;
 };
