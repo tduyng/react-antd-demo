f420ab101eb82a0398c91392c0bd4d62e1e8b7f7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-21 19:36:35 +0100

    Update Overlay pages

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,5 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Carousel } from 'antd';
+
+const initItems = [
+  {
+    key: '1',
+    title: 'Web and mobile payment built for developers',
+    content:
+      'An vim odio ocurreret consetetur, justo constituto ex mea. Quidam facilisis vituperata pri ne. Id nostrud gubergren urbanitas sed, quo summo animal qualisque ut, cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.',
+  },
+  {
+    key: '2',
+    title: 'Work better together. Schedule meetings',
+    content:
+      'An vim odio ocurreret consetetur, justo constituto ex mea. Quidam facilisis vituperata pri ne. Id nostrud gubergren urbanitas sed, quo summo animal qualisque ut, cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.',
+  },
+  {
+    key: '3',
+    title: 'The best app to increase your productivity',
+    content:
+      'An vim odio ocurreret consetetur, justo constituto ex mea. Quidam facilisis vituperata pri ne. Id nostrud gubergren urbanitas sed, quo summo animal qualisque ut, cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.',
+  },
+];
 
 export const Hero = () => {
-  return <div>Hero</div>;
+  const [items, setItems] = useState(initItems);
+
+  useEffect(() => {
+    setItems(initItems);
+    // eslint-disable-next-line no-console
+    items.forEach(x => console.log(x.title, x.content));
+  }, [items]);
+
+  return (
+    <div id="hero" className="heroBlock">
+      <Carousel autoplay>
+        {items.map(item => {
+          return (
+            <div key={item.key} className="container-fluid">
+              <div className="content">
+                <h3>{item.title}</h3>
+                <p>{item.content}</p>
+                <div className="btnHolder">
+                  <Button type="primary" size="large">
+                    Learn More
+                  </Button>
+                  <Button size="large">
+                    <i className="fas fa-desktop"></i> Watch a Demo
+                  </Button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import 'src/assets/scss/index.scss';
 import App from 'src/App/App';
 import reportWebVitals from './reportWebVitals';
